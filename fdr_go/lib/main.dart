5a77d2a8f191fae91e4693920f21d38e50fcfd24
Pablo Johnson <pablo.88j@gmail.com>
2019-08-04 22:27:22 -0500

    fix initial issue

--- fdr_go/lib/main.dart
+++ fdr_go/lib/main.dart
@@ -26,43 +26,57 @@ class MyAppState extends State<MyApp> {
   String languageCode = "en";
   String countryCode = "US";
 
-  @override
-  void initState() {
-    super.initState();
-    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
-    prefs.then((value) {
-      languageCode = value.getString("languageCode");
-      if (languageCode == null || languageCode.isEmpty) {
-        value.setString("languageCode", "en");
-      }
-      countryCode = value.getString("countryCode");
-      new FdrLocalizationsDelegate().load(Locale(languageCode, countryCode));
-    });
+  Future<Locale> _getSavedLocale() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    languageCode = prefs.getString("languageCode");
+    if (languageCode == null || languageCode.isEmpty) {
+      prefs.setString("languageCode", "en");
+      languageCode = "en";
+    }
+    countryCode = prefs.getString("countryCode");
+    if (countryCode == null || countryCode.isEmpty) {
+      prefs.setString("countryCode", "US");
+      countryCode = "US";
+    }
+    return Locale(languageCode, countryCode);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        // ... app-specific localization delegate[s] here
-        const FdrLocalizationsDelegate(),
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('en', 'US'), // English
-        const Locale('es', 'ES'), // Spanish
-        // ... other locales the app supports
-      ],
-      locale: Locale(languageCode, countryCode),
-      title: 'FDR GO',
-      theme: ThemeData(
-        primarySwatch: const MaterialColor(0xFF002664, primaryColorMap),
-        fontFamily: 'Roboto',
-      ),
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
+    return FutureBuilder(
+        future: this._getSavedLocale(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return CircularProgressIndicator();
+            case ConnectionState.done:
+            default:
+              return MaterialApp(
+                localizationsDelegates: [
+                  // ... app-specific localization delegate[s] here
+                  const FdrLocalizationsDelegate(),
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: [
+                  const Locale('en', 'US'), // English
+                  const Locale('es', 'ES'), // Spanish
+                  // ... other locales the app supports
+                ],
+                locale: snapshot.data,
+                title: 'FDR GO',
+                theme: ThemeData(
+                  primarySwatch:
+                      const MaterialColor(0xFF002664, primaryColorMap),
+                  fontFamily: 'Roboto',
+                ),
+                debugShowCheckedModeBanner: false,
+                home: SplashScreen(),
+              );
+          }
+        });
   }
 }

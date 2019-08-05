9953f81b28921da409972d6155819576a146c0a6
Pablo Johnson <pablo.88j@gmail.com>
2019-08-04 22:31:58 -0500

    Fix language issue

--- fdr_go/lib/lang/fdr_localizations_delegate.dart
+++ fdr_go/lib/lang/fdr_localizations_delegate.dart
@@ -1,16 +1,18 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'fdr_localizations.dart';
- 
+
 class FdrLocalizationsDelegate extends LocalizationsDelegate<FdrLocalizations> {
   const FdrLocalizationsDelegate();
- 
+
   @override
   bool isSupported(Locale locale) => ['en', 'es'].contains(locale.languageCode);
- 
+
   @override
   Future<FdrLocalizations> load(Locale locale) => FdrLocalizations.load(locale);
- 
+
   @override
-  bool shouldReload(LocalizationsDelegate<FdrLocalizations> old) => true;
+  bool shouldReload(LocalizationsDelegate<FdrLocalizations> old) => false;
 }
